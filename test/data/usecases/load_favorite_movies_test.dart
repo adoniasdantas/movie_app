e757b923899599373590007a6d046e3fcc6f28d5
Adonias Dantas <adoniasdantas@gmail.com>
2023-06-24 14:18:12 +0200

    refactor: remove duplication from tests

--- test/data/usecases/load_favorite_movies_test.dart
+++ test/data/usecases/load_favorite_movies_test.dart
@@ -14,28 +14,30 @@ void main() {
   late CacheLoadFavoriteMovies sut;
   late List<int> data;
 
+  When mockCacheStorageFetch() => when(() => cacheStorageSpy.fetch(any()));
+
   setUp(() {
     cacheStorageSpy = CacheStorageSpy();
     sut = CacheLoadFavoriteMovies(cacheStorage: cacheStorageSpy);
     data = faker.randomGenerator.numbers(100, 3);
+    mockCacheStorageFetch().thenAnswer((_) async => data);
   });
 
   test('Should call cacheStorage with correct values', () async {
-    when(() => cacheStorageSpy.fetch(any())).thenAnswer((_) async => data);
     await sut();
 
     verify(() => cacheStorageSpy.fetch('favorite-movies')).called(1);
   });
 
   test('Should return a list of integers on success', () async {
-    when(() => cacheStorageSpy.fetch(any())).thenAnswer((_) async => data);
     final movieIds = await sut();
 
     expect(movieIds, data);
   });
 
   test('Should throws UnexpectedError if cacheStorage throws', () async {
-    when(() => cacheStorageSpy.fetch(any())).thenThrow(Exception());
+    mockCacheStorageFetch().thenThrow(Exception());
+
     final future = sut();
 
     expect(future, throwsA(DomainError.unexpected));
@@ -43,8 +45,8 @@ void main() {
 
   test('Should throw UnexpectedError if cacheStorage returns invalid data',
       () async {
-    when(() => cacheStorageSpy.fetch(any()))
-        .thenAnswer((_) async => ["1", null]);
+    mockCacheStorageFetch().thenAnswer((_) async => ["1", null]);
+
     final future = sut();
 
     expect(future, throwsA(DomainError.unexpected));
